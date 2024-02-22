fc19b6fb0879361f87ac6dd771bcc023f0599ff9
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2024-02-21 19:05:04 -0800

    added ckeditor 5 to admin panel

--- backend/gamha_blog/api/models.py
+++ backend/gamha_blog/api/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    subtitle = models.CharField(max_length=200)
-    content = models.TextField()
+    subtitle = models.CharField(max_length=200, blank=True)
+    content = CKEditor5Field('Text', config_name='extends')
     originally_published = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
