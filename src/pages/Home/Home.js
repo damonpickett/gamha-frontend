65fef823203dd414d71f23d8d10718ca4b4eb8f4
Damon Pickett <damon.pickett@gmail.com>
2025-07-11 18:13:59 -0700

    added api call for home page intro

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,11 +4,24 @@ import axios from "axios";
 import "./Home.css";
 
 const Home = () => {
+  const [introText, setIntroText] = useState("");
   const [myBook, setMyBook] = useState(null);
   const [loading, setLoading] = useState(true);
   const [isSubscribeModalOpen, setSubscribeModalOpen] = useState(false); // Add state for the subscribe modal
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const apiUrl = process.env.REACT_APP_API_URL;
+    axios
+      .get(`${apiUrl}/api/homepage-intro/`)
+      .then((response) => {
+        setIntroText(response.data.text);
+      })
+      .catch((error) => {
+        console.error("Error fetching homepage intro:", error);
+      });
+  }, []);
 
   useEffect(() => {
     const apiUrl = process.env.REACT_APP_API_URL;
@@ -92,47 +105,7 @@ const Home = () => {
         <div className="intro">
           <h1>Introduction</h1>
           <div className="intro-text">
-          <p>Sometime in mid 2022, I cracked.</p>
-
-          <p>
-            I was living in my Dad’s windowless basement, I was unemployed, and
-            for over 2 years I only left the house to buy toothpaste.
-          </p>
-
-          <p>
-            I was trying to get a job as a software developer. I had no
-            experience, no confidence, and no real desire to work as one. I was
-            following a script that someone else had written. I thought if I
-            followed the script I’d be okay.
-          </p>
-
-          <p>
-            But in the context of a pandemic, inflation, social division,
-            information overload, and mounting tyranny, the script was beyond
-            irrelevant, it was obscene. I felt like I was drifting on the
-            surface of the ocean on a disintegrating raft, rising and
-            falling with the waves during a storm. One night, pacing back and
-            forth in my room, struggling against an invisible grip around my
-            chest, I finally admitted to myself “I need an anchor, I need
-            something more.”
-          </p>
-
-          <p>I humbled myself and opened my heart and mind to God.</p>
-
-          <p>
-            Slowly, a new world opened up to me, a world beyond the one we
-            see.
-          </p>
-
-          <p>
-            The stories I’ve written are my way of reflecting this experience to
-            myself and others.
-          </p>
-
-          <p>
-            If you’d like to be notified of new releases, click the button
-            below.
-          </p>
+            <div dangerouslySetInnerHTML={{ __html: introText }}></div>
           </div>
           <button className="home-button" onClick={openSubscribeModal}>
             Subscribe
